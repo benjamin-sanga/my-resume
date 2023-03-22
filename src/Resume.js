1afeb0d3e97693c7b79cf7efac413540e5134e01
benjaminsanga <benjamin.sanga@outsourceglobal.com>
2023-03-23 00:29:52 +0100

    init boilerplate

---
+++ src/Resume.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Page, Text, View, Document, StyleSheet, Font } from '@react-pdf/renderer';
+
+// Register font
+Font.register({ family: 'Roboto' });
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text style={styles.title}>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+export default MyDocument
